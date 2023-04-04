714ee163da00fe4b916840dfc513f1639be0feac
EarlHikky <101412518+EarlHikky@users.noreply.github.com>
2023-04-04 22:16:38 +0300

    Add Places model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Places
+
+
+class PlacesAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_display_links = ('title',)
+    search_fields = ('title',)
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Places, PlacesAdmin)
+
+admin.site.site_title = 'Админ-панель'
+admin.site.site_header = 'Админ-панель'
--- app/models.py
+++ app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.urls import reverse
+from pytils.translit import slugify
 
-print('1')
+
+class Places(models.Model):
+    title = models.CharField(max_length=100, db_index=True, verbose_name="Место")
+    description_short = models.CharField(max_length=255, verbose_name="Короткое описание")
+    description_long = models.TextField(verbose_name="Описание")
+    coordinates_lng = models.CharField(max_length=25, verbose_name="Долгота")
+    coordinates_lat = models.CharField(max_length=25, verbose_name="Широта")
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name="URL")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
+        ordering = ["title"]
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super(Places, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('place', kwargs={'place_id': self.slug})
