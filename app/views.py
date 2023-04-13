5e761a5cb95cf98a84f89544e4f10c8cb4e75f2c
EarlHikky <andrew.malofeev@gmail.com>
2023-04-13 17:13:54 +0300

    data loads from database

--- app/views.py
+++ app/views.py
@@ -1,15 +1,15 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.urls import reverse
+
 from app.models import Places, Images
 
 
 def index(request):
     places = Places.objects.all()
-    places_points = []
-    detailsUrl = ["./static/app/places/moscow_legends.json", "./static/app/places/roofs24.json"]
-
-    for i, place in enumerate(places):
-        places_points.append(
+    features = []
+    for place in places:
+        features.append(
             {
                 "type": "Feature",
                 "geometry": {
@@ -19,14 +19,14 @@ def index(request):
                 "properties": {
                     "title": place.title,
                     "placeId": place.slug,
-                    "detailsUrl": detailsUrl[i]
+                    "detailsUrl": reverse('places', args=[place.pk])
                 }
             }
         )
 
     places_geojson = {
         "type": "FeatureCollection",
-        "features": places_points
+        "features": features
     }
 
     return render(request, 'app/index.html', {'places_geojson': places_geojson})
