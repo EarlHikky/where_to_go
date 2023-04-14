3fa3e4f3db007f1aab71542ac9a33dc4d58e59ad
EarlHikky <andrew.malofeev@gmail.com>
2023-04-14 12:48:44 +0300

    environs installed

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c8szfp_koc%mp70d$(yjfvy#ojb$sj3pcdpl(knf-m_)!-v9y8'
+SECRET_KEY = env("SECRET_KEY") if env("SECRET_KEY") else "REPLACE_ME"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG") if not env.bool("DEBUG") else True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS") if env.list("ALLOWED_HOSTS") else ['127.0.0.1', 'localhost',
+                                                                             '.pythonanywhere.com']
 
 # Application definition
 
@@ -56,7 +61,7 @@ ROOT_URLCONF = 'where_to_go.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
