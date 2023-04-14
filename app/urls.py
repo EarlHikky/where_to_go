bd5c054ca3ef163b5272f36f312840321eb34f6d
EarlHikky <andrew.malofeev@gmail.com>
2023-04-14 12:23:49 +0300

    django-tinymce installed

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
     path("", index, name="index"),
     path("places/<int:place_id>/", place_view, name="places"),
+    path('tinymce/', include('tinymce.urls')),
 
 ]
