a4a00b8110d3127eb0fffa92fe5f6e84f2151248
EarlHikky <andrew.malofeev@gmail.com>
2023-04-18 14:20:38 +0300

    related_name added

--- app/models.py
+++ app/models.py
@@ -12,34 +12,34 @@ class Places(models.Model):
     coordinates_lat = models.FloatField(max_length=25, verbose_name="Широта", blank=True)
     slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name="URL")
 
-    def __str__(self):
-        return self.title
-
     class Meta:
         verbose_name = 'Место'
         verbose_name_plural = 'Места'
         ordering = ('title',)
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        return super(Places, self).save(*args, **kwargs)
+    def __str__(self):
+        return self.title
 
     def get_absolute_url(self):
         return reverse('places', kwargs={'place_id': self.pk})
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super(Places, self).save(*args, **kwargs)
 
 
 class Images(models.Model):
     def choose_folder(self, filename):
         return f'./photos/{self.place.slug}/{filename}'
 
-    place = models.ForeignKey(Places, verbose_name='Место', on_delete=models.CASCADE)
+    place = models.ForeignKey(Places, verbose_name='Место', related_name='place_images', on_delete=models.CASCADE)
     image = models.ImageField(upload_to=choose_folder, verbose_name="Фото")
     position = models.PositiveIntegerField(verbose_name='Позиция', default=0, blank=False, null=False)
-
-    def __str__(self):
-        return f'{self.position} {self.place.title}'
 
     class Meta:
         verbose_name = 'Изображение'
         verbose_name_plural = 'Изображения'
         ordering = ('position',)
+
+    def __str__(self):
+        return f'{self.position} {self.place.title}'
